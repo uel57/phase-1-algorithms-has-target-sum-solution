f9f207f6355a478eb915312561b460a65b8e3cb8
samuel <samuelkariuki1@student.moringaschool.com>
2023-06-29 06:24:51 +0300

    final solution

--- index.js
+++ index.js
@@ -1,18 +1,136 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+// }
+
+// /* 
+//   Write the Big O time complexity of your function here
+// */
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+//   console.log("");
+
+//   console.log("Expecting: true");
+//   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", hasTargetSum([1, 2, 5], 4));
+// }
+
+// module.exports = hasTargetSum;
+
+
+//rewriting solution in my own words
+
+//I need to make a hasTargetSum function that checks if two numbers in an array add up to some target number. 
+//For example, if the array is [1,2,3,4] and the target number is 6, I know that 2 and 4 add up to 6, so I should return true. 
+//If I have the same array and the target is 10, no two numbers in the array add up to 10, so I should return false.
+
+
+//test case
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// PseudoCode
 
-/* 
-  Add your pseudocode here
-*/
+// iterate over the array of numbers
+//   for the current number, identify a complementary number that adds to our target
+//   (for example: if our number is 2, and the target is 5, the complementary number is 3)
+//   iterate over the remaining numbers in the array
+//     check if any of the remaining numbers is the complement
+//       if so, return true
+// if we reach the end of the array, return false
 
-/*
-  Add written explanation of your solution here
-*/
+
+// Code
+
+function hasTargetSum(array, target) {
+  // iterate over the array of numbers
+  for (let i = 0; i < array.length; i++) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+  for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  // if we reach the end of the array, return false
+  return false;
+}
+
+// make it readable 
+
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
+}
+
+//Optimize
+function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
